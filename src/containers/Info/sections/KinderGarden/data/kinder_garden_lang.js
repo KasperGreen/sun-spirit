7fc778c7f7a8bad5ab00ded0ae3c9ebabe06a3e5
Kasper Green <kaspergreen@gmail.com>
2018-05-06 21:32:19 +0300

    KinderGarden Lang

---
+++ src/containers/Info/sections/KinderGarden/data/kinder_garden_lang.js
@@ -0,0 +1,78 @@
+import { LANG_EN, LANG_RU } from 'constants/LANG'
+import React from 'react'
+
+export default {
+  left: {
+    [LANG_RU]: <div>
+      <p>Проект <b>&laquo;KINDER SPACE&raquo;</b> рад пригласить Вас и&nbsp;Ваших детей на
+        природный музыкальный эко-фестиваль, который пройдёт в&nbsp;самых живописных горах нашей страны. &laquo;SUN
+        SPIRIT&raquo;&nbsp;&mdash; это семейный фестиваль,
+        который объединит людей, которые любят развиваться, самосовершенствоваться, ведут здоровый образ жизни и
+        танцуют под красивую музыку.</p>
+      <p>В&nbsp;походных условиях мы&nbsp;постараемся сделать наше пространство как всегда комфортным и&nbsp;безопасным
+        для
+        вашего ребёнка, с&nbsp;увлекательной программой и&nbsp;с&nbsp;внимательным и&nbsp;проверенным штатом добрых
+        волшебниц.
+        Путешествие для вашего ребёнка обещает быть очень насыщенным&nbsp;&mdash; сейчас мы&nbsp;прорабатываем новое
+        наполнение
+        и&nbsp;увлекательную программу для деток.</p>
+      <p>Программа будет проходить на&nbsp;территории &laquo;KINDER SPACE&raquo; в&nbsp;течении всего дня, каждый день
+        будет посвящён
+        развитию ребенка физически, творчески и&nbsp;интеллектуально. Вы&nbsp;всегда можете оставить своего самого
+        маленького ребёнка под присмотр профессиональной няни.</p></div>,
+    [LANG_EN]: <div><p><strong>Kinder Space</strong> project is&nbsp;inviting you and your kids to&nbsp;join
+      a&nbsp;picturesque music eco festival in
+      one of&nbsp;the most breathtaking sights of&nbsp;Russia.
+    </p>
+      <p><strong>Sun Spirit</strong> is&nbsp;a&nbsp;family festival uniting the ones who strive for self development and
+        perfection, leading healthy life style and dancing to&nbsp;beautiful music.
+      </p>
+      <p>In&nbsp;terms of&nbsp;the festival we&nbsp;will do&nbsp;our best to&nbsp;create safe and comfortable
+        environment for your children,
+        there will be&nbsp;entertaining programs and kind attentive fairy staff working with them. The journey for your
+        kid is
+        going to&nbsp;be&nbsp;rich in&nbsp;physical, creative and intellectual activities.
+      </p>
+      <p>Программа будет проходить на&nbsp;территории &laquo;KINDER SPACE&raquo; в&nbsp;течении всего дня, каждый день
+        будет посвящён
+        развитию ребенка физически, творчески и&nbsp;интеллектуально. Вы&nbsp;всегда можете оставить своего самого
+        маленького ребёнка под присмотр профессиональной няни.</p>
+    </div>
+  },
+  right: {
+    [LANG_RU]: <div>
+
+      <h4>Всегда в нашем пространстве: </h4>
+      <ul>
+        <li>Питьевая вода</li>
+        <li>Детская йога</li>
+        <li>Творческие мастер-классы</li>
+        <li>Арт-терапия «МАНДАЛА»</li>
+        <li>Игровое пространство</li>
+        <li>Музыкальные джемы</li>
+        <li>Уроки кручения пои и хулахупа</li>
+        <li>Красочный аквагримм</li>
+        <li>И МУЛЬТИКИ!</li>
+      </ul>
+      <p></p>
+      <p><b>Работа площадки с 10.00 &nbsp;до 24.00 по расписанию.</b></p>
+    </div>,
+    [LANG_EN]: <div>
+
+      <h4>Our kids space is providing:</h4>
+      <ul>
+        <li>Potable water;</li>
+        <li>Yoga for kids;</li>
+        <li>Arts and Crafts;</li>
+        <li>Mandala art-therapy;</li>
+        <li>Gaming space;</li>
+        <li>Music jams;</li>
+        <li>Poi and hula hoop classes;</li>
+        <li>Face-painting;</li>
+        <li>Cartoooons!</li>
+      </ul>
+      <p></p>
+      <p><b>Работа площадки с 10.00 &nbsp;до 24.00 по расписанию.</b></p>
+    </div>
+  }
+}
