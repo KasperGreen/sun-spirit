9a842e3c872966fe74fae88e71ef6c769b827bd1
Kasper Green <kaspergreen@gmail.com>
2018-05-06 21:20:23 +0300

    InstallationsAndExhibitions lang

--- src/containers/Info/sections/InstallationsAndExhibitions/InstallationsAndExhibitions.jsx
+++ src/containers/Info/sections/InstallationsAndExhibitions/InstallationsAndExhibitions.jsx
@@ -3,27 +3,25 @@ import './InstallationsAndExhibitions.css'
 import instalation_image_1 from './images/install1.jpg'
 import instalation_image_2 from './images/install2.jpg'
 import instalation_image_3 from './images/install3.jpg'
+import text from './data/installations_and_exhibitions_lang'
 
 export default class InstallationsAndExhibitions extends Component {
   render () {
+    const {
+      props: {
+        lang
+      }
+    } = this
     return (
       <div className='InstallationsAndExhibitions'>
 
         <div className="cols">
-          <div className="col_left50"><p><b>Фестиваль – это место для творческого самовыражения!</b></p>
-            <p>Sun spirit Festival - большая арт инсталляция, художественно проработанные элементы которой несут
-              в себе свои уникальные образы, заложенные их идеологами.</p>
-            <p>Здесь театрализованные шоу, концептуальные постановки,
-              спонтанные и отточенные представления мастеров мистерий будут удивлять гостей то появляясь,
-              то исчезая на просторах фестиваля, каждый раз преподнося новые образы и движения вниманию зрителя.</p>
+          <div className="col_left50">
+            {text.left[lang]}
           </div>
           <div className="col_right50">
-
-            <p>Здесь создаются тематические арт-инсталляции и постройки командами-участниками фестиваля,
-              а также будет открыт WORKSHOP по созданию совместной ключевой арт-инсталляции для всех желающих
-              фестиваля - «ЕДИНЕНИЕ».</p>
-            <p>Здесь художники демонстрируют свое творчество, творят в режиме реального времен,
-              показывая публике живой процесс рождения картин.</p></div>
+            {text.right[lang]}
+          </div>
         </div>
         <div className="InfoSections-flex_images">
           <div>
---
+++ src/containers/Info/sections/InstallationsAndExhibitions/data/installations_and_exhibitions_lang.js
@@ -0,0 +1,29 @@
+import { LANG_EN, LANG_RU } from 'constants/LANG'
+import React from 'react'
+
+export default {
+  left: {
+    [LANG_RU]: <div><p><b>Фестиваль&nbsp;&mdash; это место для творческого самовыражения!</b></p>
+      <p>Sun spirit Festival&nbsp;&mdash; большая арт инсталляция, художественно проработанные элементы которой несут
+        в&nbsp;себе свои уникальные образы, заложенные их&nbsp;идеологами.</p>
+      <p>Здесь театрализованные шоу, концептуальные постановки,
+        спонтанные и&nbsp;отточенные представления мастеров мистерий будут удивлять гостей то&nbsp;появляясь,
+        то&nbsp;исчезая на&nbsp;просторах фестиваля, каждый раз преподнося новые образы и&nbsp;движения вниманию зрителя.</p></div>,
+    [LANG_EN]: <div><h4>The festival is&nbsp;the right place for creative self realization!</h4>
+      <p>Sun spirit Festival is&nbsp;a&nbsp;big art installation, decorative elements of&nbsp;which convey unique images brought by
+        visionary artists. </p>
+      <p>Concept performances, theatrical shows, spontaneous and well-prepaired shows by&nbsp;masters of&nbsp;mystery will
+        surprise you, appearing and vanishing somewhere at&nbsp;the Festival. Every time they will bring new images and
+        motions to&nbsp;your attention</p></div>
+  },
+  right: {
+    [LANG_RU]: <div>
+      <p>Здесь создаются тематические арт-инсталляции и&nbsp;постройки командами-участниками фестиваля,
+        а&nbsp;также будет открыт WORKSHOP по&nbsp;созданию совместной ключевой арт-инсталляции для всех желающих
+        фестиваля&nbsp;&mdash; &laquo;ЕДИНЕНИЕ&raquo;.</p>
+      <p>Здесь художники демонстрируют свое творчество, творят в&nbsp;режиме реального времен,
+        показывая публике живой процесс рождения картин.</p></div>,
+    [LANG_EN]: <div>Здесь художники демонстрируют свое творчество, творят в&nbsp;режиме реального времен,
+      показывая публике живой процесс рождения картин.</div>
+  }
+}
