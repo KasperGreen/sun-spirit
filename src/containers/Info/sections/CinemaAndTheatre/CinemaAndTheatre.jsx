5bbe034f9b1133d8086a8807eff8a7dd97181557
Kasper Green <kaspergreen@gmail.com>
2018-05-06 21:13:51 +0300

    Cinema And Theatre Lang

--- src/containers/Info/sections/CinemaAndTheatre/CinemaAndTheatre.jsx
+++ src/containers/Info/sections/CinemaAndTheatre/CinemaAndTheatre.jsx
@@ -3,16 +3,21 @@ import './CinemaAndTheatre.css'
 import cinema1 from './images/kinoiteatr1.jpg'
 import cinema2 from './images/kinoiteatr2.jpg'
 import cinema3 from './images/kinoiteatr3.jpg'
+import text from './data/cinema_and_theatre_lang'
 
 export default class CinemaAndTheatre extends Component {
   render () {
+    const {
+      props: {
+        lang
+      }
+    } = this
     return (
       <div className='CinemaAndTheatre'>
 
-        <div className="cols"><p>Кинотеатр – уникальное пространство интеллектуального и неформатного, местами
-          специфического, но не лишенного смысла, кино. В рамках кинотеатра режиссеры и сценаристы покажут Вам свои
-          работы и пригласят к обсуждению идей и смыслов, заложенных в каждой из них, будут проведены курсы по
-          актерскому мастерству. </p></div>
+        <div className="cols">
+          {text.top[lang]}
+        </div>
         <div className="InfoSections-flex_images">
           <div><img src={cinema1} alt={'Foto'} /></div>
           <div><img src={cinema2} alt={'Foto'} /></div>
---
+++ src/containers/Info/sections/CinemaAndTheatre/data/cinema_and_theatre_lang.js
@@ -0,0 +1,20 @@
+import { LANG_EN, LANG_RU } from 'constants/LANG'
+import React from 'react'
+
+export default {
+  top: {
+    [LANG_RU]: <div>
+      <p>Кинотеатр – уникальное пространство интеллектуального и неформатного, местами
+        специфического, но не лишенного смысла, кино. В рамках кинотеатра режиссеры и сценаристы покажут Вам свои
+        работы и пригласят к обсуждению идей и смыслов, заложенных в каждой из них, будут проведены курсы по
+        актерскому мастерству. </p>
+    </div>,
+    [LANG_EN]: <div>
+      <p>
+        Cinema is a special space with intellectual and extraordinary, at times odd, but making sense films. Withing the
+        framework of the festival directors and screenwriters will show you their artwork and encourage you to discuss
+        ideas and message of the films. There will also be drama courses held.
+      </p>
+    </div>
+  }
+}
