e8c698f7b529e19fa947a17daffc43acc57d1eb2
Kasper Green <kaspergreen@gmail.com>
2018-05-06 19:18:26 +0300

    Redirect if 404

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Main from 'containers/Main'
 import 'css/bootstrap.min.css'
 import 'css/style-2018.css'
@@ -47,6 +47,7 @@ class App extends Component {
               <Route path={'/info/:section?'} component={Info} />
               <Route path={'/noosphere/:lesson?'} component={Noosphere} />
               <Route path={'/music/:stage?/:artist?'} component={Music} />
+              <Redirect to={'/'}/>
             </Switch>
             <AppUpdateNotifier />
           </div>
