4286eddbf5b4d10a46f1c8aa4e8a2c9a5c4ce112
Kasper Green <kaspergreen@gmail.com>
2018-05-06 21:08:33 +0300

    Art-Market lang

--- src/containers/Info/sections/ArtMarket/ArtMarket.jsx
+++ src/containers/Info/sections/ArtMarket/ArtMarket.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 import './ArtMarket.css'
 import art_market_1 from './images/artmarket1.jpg'
 import art_market_2 from './images/artmarket2.jpg'
-
+import text from './data/art_market_lang'
 export default class ArtMarket extends Component {
   render () {
+    const {
+      props: {
+        lang
+      }
+    } = this
     return (
       <div className='ArtMarket'>
 
-        <div className="cols"><p>Это уникальное пространство фестиваля, где дизайнеры, путешественники, ремесленники
-          дают возможность гостям приобрести уникальные вещи собственного дизайнерского производства и уникальные
-          декоративные артефакты с разных уголков планеты, которые не найти в обычных магазинах.
-          Здесь творят и мастерят, делятся опытом умельцы народных промыслов.</p></div>
+        <div className="cols">
+          {text.top[lang]}
+        </div>
         <div className="InfoSections-flex_images">
           <div><img src={art_market_1} alt={'Foto'} /></div>
           <div><img src={art_market_2} alt={'Foto'} /></div>
---
+++ src/containers/Info/sections/ArtMarket/data/art_market_lang.js
@@ -0,0 +1,22 @@
+import { LANG_EN, LANG_RU } from 'constants/LANG'
+import React from 'react'
+
+export default {
+  top: {
+    [LANG_RU]: <div>
+      <p>
+        Это уникальное пространство фестиваля, где дизайнеры, путешественники, ремесленники
+        дают возможность гостям приобрести уникальные вещи собственного дизайнерского производства и уникальные
+        декоративные артефакты с разных уголков планеты, которые не найти в обычных магазинах.
+        Здесь творят и мастерят, делятся опытом умельцы народных промыслов.
+      </p>
+    </div>,
+    [LANG_EN]: <div>
+      <p>
+        It's a unique festival space where designers, travellers, craftsmen offer the visitors one-of-a-kind things made
+        by their hands and unique decorative artefacts from all over the world. You won’t be able to find it in regular
+        shops. It’s a space of creativity, artistry and experience sharing from craftsmen.
+      </p>
+    </div>
+  }
+}
