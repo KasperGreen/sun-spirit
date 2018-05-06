8415dead0e1c80eafa2fcb9f529163c24ecc61f8
Kasper Green <kaspergreen@gmail.com>
2018-05-06 04:05:29 +0300

    Main Linear gradient

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -6,7 +6,7 @@ export default class Main extends Component {
 
   render () {
     return (
-      <PageWrapper footer_style={{background: '#402D41', boxShadow: 'none'}}>
+      <PageWrapper footer_style={{background: 'linear-gradient(rgb(64, 45, 65), rgb(62, 29, 64))', boxShadow: 'none'}}>
         <MainLanding />
       </PageWrapper>
     )
