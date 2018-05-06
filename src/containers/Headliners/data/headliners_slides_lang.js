779ed9926f25a574e076a08b98b867c321dda295
Kasper Green <kaspergreen@gmail.com>
2018-05-06 21:48:18 +0300

    typography headliners

--- src/containers/Headliners/data/headliners_slides_lang.js
+++ src/containers/Headliners/data/headliners_slides_lang.js
@@ -17,20 +17,20 @@ export default [
     },
     player_url: 'music/atmosphere/kalya-scintilia-and-eve-olution',
     description: {
-      [LANG_RU]: <div>
-        Сегодня Kalya Scintilla - известный на всех континентах планеты музыкальный проект Австралийского
-        продюсера, продолжающий набирать популярность и выступающий на крупных фестивалях США и Европы.
+      [LANG_RU]: <div>Сегодня Kalya Scintilla&nbsp;&mdash; известный на&nbsp;всех континентах планеты музыкальный проект
+        Австралийского
+        продюсера, продолжающий набирать популярность и&nbsp;выступающий на&nbsp;крупных фестивалях США и&nbsp;Европы.
 
-        В 2012 рождается творческий союз Kalya Scintilla и Eve Olution (театральной художницы из США),
-        совместное творчество которых транслирует через музыку и театр чистую гармонию жизни, архетипичные
-        выражения бытия.
-      </div>,
-      [LANG_EN]: <div>
-        Kalya Scintilla is an all-continent well-known music project by an Australian producer that is growing popular
-        and performing at big festivals of the USA and Europe. In 2012 a creative union of Kalya Scintilla and Eve
-        Olution (an American theatre artist) is born. Their collaboration is conveying pure life harmony and archetypal
-        expression of being by means of music and drama.
-      </div>
+        В&nbsp;2012 рождается творческий союз Kalya Scintilla и&nbsp;Eve Olution (театральной художницы из&nbsp;США),
+        совместное творчество которых транслирует через музыку и&nbsp;театр чистую гармонию жизни, архетипичные
+        выражения бытия.</div>,
+      [LANG_EN]: <div>Kalya Scintilla is&nbsp;an&nbsp;all-continent well-known music project by&nbsp;an&nbsp;Australian
+        producer that is&nbsp;growing popular
+        and performing at&nbsp;big festivals of&nbsp;the USA and Europe. In&nbsp;2012&nbsp;a creative union
+        of&nbsp;Kalya Scintilla and Eve
+        Olution (an&nbsp;American theatre artist) is&nbsp;born. Their collaboration is&nbsp;conveying pure life harmony
+        and archetypal
+        expression of&nbsp;being by&nbsp;means of&nbsp;music and drama.</div>
     }
   },
   {
@@ -42,17 +42,23 @@ export default [
     },
     description: {
       [LANG_RU]: <div>
-        Martin Nonstatic (Ultimae rec.) начал работу со звуком в 18 лет, экспериментируя с различными
-        инструментами и окружающими звуками. Он довольно долго занимался живыми выступлениями на различных
-        мероприятиях и созданием треков. В 2009 году Martin посвящает себя студийной. Его музыка с тех пор
-        получила международную поддержку от разных лейблов, таких как Bine Music, Silent Season, Dewtone,
-        Ultimae и Sub.spiele.
+        Martin Nonstatic (Ultimae rec.) начал работу со&nbsp;звуком в&nbsp;18&nbsp;лет, экспериментируя
+        с&nbsp;различными
+        инструментами и&nbsp;окружающими звуками. Он&nbsp;довольно долго занимался живыми выступлениями
+        на&nbsp;различных
+        мероприятиях и&nbsp;созданием треков. В&nbsp;2009 году Martin посвящает себя студийной. Его музыка с&nbsp;тех
+        пор
+        получила международную поддержку от&nbsp;разных лейблов, таких как Bine Music, Silent Season, Dewtone,
+        Ultimae и&nbsp;Sub.spiele.
       </div>,
       [LANG_EN]: <div>
-        Martin Nonstatic (Ultimae rec.) started working with sound engineering at the age of 18, experimenting with
-        different instruments and surrounding sounds. Quite some time he was engaged in work at live performances of
-        different events and tracks creating. In 2009 Martin devoted himself to studio work. Since then his music’s been
-        supported by different labels like Bine Music, Silent Season, Dewtone, Ultimae and Sub.spiele.
+        Martin Nonstatic (Ultimae rec.) started working with sound engineering at&nbsp;the age of&nbsp;18, experimenting
+        with
+        different instruments and surrounding sounds. Quite some time he&nbsp;was engaged in&nbsp;work at&nbsp;live
+        performances of
+        different events and tracks creating. In&nbsp;2009 Martin devoted himself to&nbsp;studio work. Since then his
+        music&rsquo;s been
+        supported by&nbsp;different labels like Bine Music, Silent Season, Dewtone, Ultimae and Sub.spiele.
       </div>
     }
   },
@@ -65,17 +71,20 @@ export default [
     },
     description: {
       [LANG_RU]: <div>
-        Австралийский проект стартовал в 2009 году на лейбле Zenon rec, в последствии сформировавшись в
-        самостоятельный лейбл Merkaba music, известный во всем мире, на котором издается множество
+        Австралийский проект стартовал в&nbsp;2009 году на&nbsp;лейбле Zenon rec, в&nbsp;последствии сформировавшись в
+        самостоятельный лейбл Merkaba music, известный во&nbsp;всем мире, на&nbsp;котором издается множество
         музыкальных проектов.
-        Уникальное и органичное звучание проекта Merkaba вызывает к пробуждению забытого древнего племенного
-        духа, скрытого в нашей клеточной памяти, и к бесконечной силе во всех нас.
+        Уникальное и&nbsp;органичное звучание проекта Merkaba вызывает к&nbsp;пробуждению забытого древнего племенного
+        духа, скрытого в&nbsp;нашей клеточной памяти, и&nbsp;к&nbsp;бесконечной силе во&nbsp;всех нас.
       </div>,
       [LANG_EN]: <div>
-        This Australian project started in 2009 on Zenon rec. label, later on turned into a separate world-famous
-        Merkaba music label. The label releases loads of music projects. Unique and organic sound of Merkaba project
-        calls you to evoke a lost ancient tribe spirit, hidden in our cellular memory and to the endless power inside
-        all of us.
+        This Australian project started in&nbsp;2009&nbsp;on Zenon rec. label, later on&nbsp;turned into a&nbsp;separate
+        world-famous
+        Merkaba music label. The label releases loads of&nbsp;music projects. Unique and organic sound of&nbsp;Merkaba
+        project
+        calls you to&nbsp;evoke a&nbsp;lost ancient tribe spirit, hidden in&nbsp;our cellular memory and to&nbsp;the
+        endless power inside
+        all of&nbsp;us.
       </div>
     }
   },
@@ -88,16 +97,20 @@ export default [
     },
     description: {
       [LANG_RU]: <div>
-        Slackbaba создал каталог вневременных треков и продолжает впечатлять аудиторию мира с его уникальной
-        смесью звуковых стилей и жанровым изгибом мелодии.
-        После выхода из его четвертого студийного альбома, Reflections, Slackbaba приносит нам много
-        ожидаемой новой музыки в 2018 году и отмечает последнюю главу в истории его уникального музыкального
+        Slackbaba создал каталог вневременных треков и&nbsp;продолжает впечатлять аудиторию мира с&nbsp;его уникальной
+        смесью звуковых стилей и&nbsp;жанровым изгибом мелодии.
+        После выхода из&nbsp;его четвертого студийного альбома, Reflections, Slackbaba приносит нам много
+        ожидаемой новой музыки в&nbsp;2018 году и&nbsp;отмечает последнюю главу в&nbsp;истории его уникального
+        музыкального
         путешествия.
       </div>,
       [LANG_EN]: <div>
-        Slackbaba's created a catalogue of timeless tracks and keeps on impressing world audience with his unique
-        combination of sound styles and genre melody curve. After release of his fourth studio album “Reflections”
-        Slackbaba gave us tons of fresh and long-awaited music in 2018 and now is marking the final chapter of his
+        Slackbaba&rsquo;s created a&nbsp;catalogue of&nbsp;timeless tracks and keeps on&nbsp;impressing world audience
+        with his unique
+        combination of&nbsp;sound styles and genre melody curve. After release of&nbsp;his fourth studio
+        album &laquo;Reflections&raquo;
+        Slackbaba gave&nbsp;us tons of&nbsp;fresh and long-awaited music in&nbsp;2018 and now is&nbsp;marking the final
+        chapter of&nbsp;his
         unique music journey story.
       </div>
     }
@@ -111,17 +124,21 @@ export default [
     },
     description: {
       [LANG_RU]: <div>
-        Steppa Style начал заниматься рагга-джангл и регги/денсхолл музыкой в
-        2005 году. В 2010 году был признан лучшим
-        Регги/Денсхолл Артистом России по верчии DHQ. В 2015 году на волнах радио Мегаполис впервые
-        прозвучала передача Good Vibes от Steppa Style и Mr Kingston. На фестивале Steppa Style выступит с
-        одним из лучших регги-бэндов страны - The Stereodrop.
+        Steppa Style начал заниматься рагга-джангл и&nbsp;регги/денсхолл музыкой в
+        2005&nbsp;году. В&nbsp;2010 году был признан лучшим
+        Регги/Денсхолл Артистом России по&nbsp;верчии DHQ. В&nbsp;2015 году на&nbsp;волнах радио Мегаполис впервые
+        прозвучала передача Good Vibes от&nbsp;Steppa Style и&nbsp;Mr&nbsp;Kingston. На&nbsp;фестивале Steppa Style
+        выступит с
+        одним из&nbsp;лучших регги-бэндов страны&nbsp;&mdash; The Stereodrop.
       </div>,
       [LANG_EN]: <div>
-        Steppa Style got engaged in ragga jungle and reggae/dancehall music in 2005. In 2010 he was considered the top
-        reggae/dancehall artist of Russia according to DHQ. In 2015 Steppa Style and Mr Kingston released the first
-        broadcast Good Vibes on Megapolis radiostation. Steppa Style will perform at the festival together with The
-        stereodrop, one of the best Russian reggae bands.
+        Steppa Style got engaged in&nbsp;ragga jungle and reggae/dancehall music in&nbsp;2005. In&nbsp;2010&nbsp;he was
+        considered the top
+        reggae/dancehall artist of&nbsp;Russia according to&nbsp;DHQ. In&nbsp;2015 Steppa Style and Mr&nbsp;Kingston
+        released the first
+        broadcast Good Vibes on&nbsp;Megapolis radiostation. Steppa Style will perform at&nbsp;the festival together
+        with The
+        stereodrop, one of&nbsp;the best Russian reggae bands.
       </div>
     }
   },
@@ -134,15 +151,19 @@ export default [
     },
     description: {
       [LANG_RU]: <div>
-        Коля Маню, безусловно, один из самых актуальных современных регги-артистов. Его музыку можно смело
-        рекомендовать всем, кто любит регги, дэнсхолл, раггамаффин и хип-хоп. Когда слушаешь треки Коли
-        Маню, на мгновение забываешь, что он из Тольятти - настолько аутентично и по-ямайски солнечно звучат
-        его песни. Он несёт людям правильные мысли и люди любят его за это.
+        Коля Маню, безусловно, один из&nbsp;самых актуальных современных регги-артистов. Его музыку можно смело
+        рекомендовать всем, кто любит регги, дэнсхолл, раггамаффин и&nbsp;хип-хоп. Когда слушаешь треки Коли
+        Маню, на&nbsp;мгновение забываешь, что он&nbsp;из&nbsp;Тольятти&nbsp;&mdash; настолько аутентично
+        и&nbsp;по-ямайски солнечно звучат
+        его песни. Он&nbsp;несёт людям правильные мысли и&nbsp;люди любят его за&nbsp;это.
       </div>,
       [LANG_EN]: <div>
-        Kolya Manyu is definetely one of the most actual modern reggae artists. You are going to love his music if you
-        are keen on reggae, dancehall, raggamuffin and hip-hop. When listening to Kolya Manyu’s tracks you forget he is
-        from Tolyatti, his songs are so steeped in authentic Jamaican sunny spirit. He is conveying the right message
+        Kolya Manyu is&nbsp;definetely one of&nbsp;the most actual modern reggae artists. You are going to&nbsp;love his
+        music if&nbsp;you
+        are keen on&nbsp;reggae, dancehall, raggamuffin and hip-hop. When listening to&nbsp;Kolya Manyu&rsquo;s tracks
+        you forget he&nbsp;is
+        from Tolyatti, his songs are so&nbsp;steeped in&nbsp;authentic Jamaican sunny spirit. He&nbsp;is&nbsp;conveying
+        the right message
         and people love him for that.
       </div>
     }
@@ -155,19 +176,23 @@ export default [
     },
     description: {
       [LANG_RU]: <div>
-        Создатель легендарной группы "Farlanders", чья музыка явилась сочетанием элементов рока, джаза и
-        разнообразных этно традиций. Группа успешно выступила на крупнейшем в мире форуме этнической музыки
-        WOMEX в Стокгольме в 1998 году. Новый состав группы Инны Желанной: басист Сергей "Grebstel" Калачев,
-        саксофонист Олег Маряхин, барабанщик Дмитрий Фролов. Музыка группы - это потрясающая смесь рока,
+        Создатель легендарной группы &laquo;Farlanders&raquo;, чья музыка явилась сочетанием элементов рока, джаза и
+        разнообразных этно традиций. Группа успешно выступила на&nbsp;крупнейшем в&nbsp;мире форуме этнической музыки
+        WOMEX в&nbsp;Стокгольме в&nbsp;1998&nbsp;году. Новый состав группы Инны Желанной: басист
+        Сергей &laquo;Grebstel&raquo; Калачев,
+        саксофонист Олег Маряхин, барабанщик Дмитрий Фролов. Музыка группы&nbsp;&mdash; это потрясающая смесь рока,
         этники, электроники, джаза.
       </div>,
       [LANG_EN]: <div>
-        The creator of legendary “Farlanders”, which music is a fusion of rock, jazz elements and different ethno
-        traditions. In 1998 the band performed successfully at WOMEX, the biggest world forum of ethno music in
-        Stockholm. The new line-up of Inna Zhelannaya’s band consists of Sergey “Grebstel” Kalachyov (bass), Oleg
-        Maryakhin (saxophone), Dmitry Frolov (drums). The band’s music is made up of a thrilling mix of rock, ethno,
+        The creator of&nbsp;legendary &laquo;Farlanders&raquo;, which music is&nbsp;a&nbsp;fusion of&nbsp;rock, jazz
+        elements and different ethno
+        traditions. In&nbsp;1998 the band performed successfully at&nbsp;WOMEX, the biggest world forum of&nbsp;ethno
+        music in
+        Stockholm. The new line-up of&nbsp;Inna Zhelannaya&rsquo;s band consists
+        of&nbsp;Sergey &laquo;Grebstel&raquo; Kalachyov (bass), Oleg
+        Maryakhin (saxophone), Dmitry Frolov (drums). The band&rsquo;s music is&nbsp;made
+        up&nbsp;of&nbsp;a&nbsp;thrilling mix of&nbsp;rock, ethno,
         electro and jazz.
-
       </div>
     }
   }
