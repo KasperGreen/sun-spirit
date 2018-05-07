621135ac3440e93d7e9da051624997045de5907c
Kasper Green <kaspergreen@gmail.com>
2018-05-07 22:49:39 +0300

    Additionally english text

--- src/containers/Info/sections/Additionally/data/additionally_lang.js
+++ src/containers/Info/sections/Additionally/data/additionally_lang.js
@@ -29,12 +29,11 @@ export default {
       <p>The festival spreads the message of&nbsp;interaction with nature and contemplating&nbsp;it, not affecting its
         integrity, encourages spiritual enrichment received from interaction with nature. </p>
       <p></p>
-      <p>Цель продвижения экотуризма в&nbsp;формате фестиваля&nbsp;&mdash; это привлечение местных жителей
-        и&nbsp;получение ими преимуществ от&nbsp;своей деятельности, обеспечивая нужды фестиваля,
-        без ущерба природе с&nbsp;сохранением ценностей местности!</p>
+      <p>Within the framework of&nbsp;the festival the aim of&nbsp;promoting ecotourism is&nbsp;to&nbsp;involve locals.
+        They can benefit from their activities supporting the festival needs, harmless to&nbsp;the nature and preserving
+        the local area.</p>
 
-      <p>Привлечение местных жителей для реализации на&nbsp;фестивале здорового экологически
-        чистого питания гостей фестиваля и&nbsp;местных товаров ремесла.</p>
+      <p>We&nbsp;involve local people to&nbsp;provide healthy ecological catering and local crafts for visitors.</p>
     </div>
   },
   right: {
@@ -50,16 +49,13 @@ export default {
         такие как ходьба по&nbsp;углям, лежание на&nbsp;гвоздях, и&nbsp;многое-многое другое.</p>
     </div>,
     [LANG_EN]: <div>
-      <p><b>Образование</b></p>
-      <p>Интересные лекторы, преподаватели, мастера,
-        ученые и учителя поделятся с Вами на лекциях и мастер-классах знаниями и опытом,
-        интересно расскажут о фундаментальных законах точных и гуманитарных наук.</p>
-      <p><b>Здоровье</b></p>
-      <p>Фестиваль Sun Spirit пропагандирует осознанный и здоровый образ жизни.
-        Наша команда мастеров здоровья предложит Вам пройти сеансы массажа,
-        оздоровительной гимнастики, спа, покажет и проведет при желании различные практики,
-        такие как ходьба по углям, лежание на гвоздях, и многое-многое другое.</p>
-
+      <p><b>Education</b></p>
+      <p>Education Interesting lecturers, teachers, handymen, scientists and tutors will share with you their knowledge
+        and skills, tell about fundamental laws of&nbsp;science and arts at&nbsp;the lectures and master-classes.</p>
+      <p><b>Health</b></p>
+      <p>The festival Sun Spirit encourages conscious and healthy lifestyle. Our team of&nbsp;health specialists
+        will offer you massage sessions, recreational gymnastics and spa, they will show you and conduct different
+        practices like firewalk, lying on&nbsp;a&nbsp;bed of&nbsp;nails and what&rsquo;s not.</p>
     </div>
   }
 }
